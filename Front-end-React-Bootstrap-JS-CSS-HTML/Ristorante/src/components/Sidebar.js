4df88d6b217f52a2e7d84d3cf0bebdae708d624c
erdifree <123300447+erdifree@users.noreply.github.com>
2023-03-17 16:44:29 +0100

    Erdison Dosti

--- Front-end-React-Bootstrap-JS-CSS-HTML/Ristorante/src/components/Sidebar.js
+++ Front-end-React-Bootstrap-JS-CSS-HTML/Ristorante/src/components/Sidebar.js
@@ -9,50 +9,49 @@ import { AiFillSetting } from "react-icons/ai";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../components/css/footerStyle.css"
+import "../components/css/footerStyle.css";
 
-export const Sidebar = ({ }) => {
+export const Sidebar = ({}) => {
   const [show, setShow] = useState(true);
 
-  const autenticazione=(param)=>{
-    const pin= [1,4,1,8]
-    if(param===pin){
-      return true
-    }
-    else {
+  const autenticazione = (param) => {
+    const pin = [1, 4, 1, 8];
+    if (param === pin) {
+      return true;
+    } else {
       return false;
     }
-  }
+  };
 
   return (
-    <header>
-      <Navbar bg="dark" className=" justify-content-between  fixed-top mb-5" >
-        <Col sx={12} lg={2} className="mt-2">
-          <Link to="/">
-            <Button variant="outline-success">
-              <AiFillSetting />
-              Home
-            </Button>
-          </Link>
-        </Col>
-
-        <Col sx={12} lg={2} className="mt-2">
-          <Link to="/admin">
-            <Button variant="outline-success">
-              <AiFillSetting />
-              Admin
-            </Button>
-          </Link>
-        </Col>
-        <Col sx={12} lg={2} className="mt-2  ms-5">
-          <Link to="/table">
-            <Button variant="outline-success">
-              <AiFillSetting />
-              Tavoli
-            </Button>
-          </Link>
-        </Col>
-      </Navbar>
-    </header>
+    <Navbar
+      bg="dark"
+      className=" fixed-top mb-5 d-flex justify-content-between"
+    >
+      <Col sx={12} lg={2} className=" d-flex justify-content-center">
+        <Link to="/">
+          <Button variant="outline-success ">
+            <AiFillSetting />
+            Home
+          </Button>
+        </Link>
+      </Col>
+      <Col sx={12} sm={3} lg={2} className="d-flex justify-content-center">
+        <Link to="/admin">
+          <Button variant="outline-success">
+            <AiFillSetting />
+            Admin
+          </Button>
+        </Link>
+      </Col>
+      <Col sx={12} lg={2} className="d-flex justify-content-center">
+        <Link to="/table">
+          <Button variant="outline-success">
+            <AiFillSetting />
+            Tavoli
+          </Button>
+        </Link>
+      </Col>
+    </Navbar>
   );
 };
--- Front-end-React-Bootstrap-JS-CSS-HTML/Ristorante/src/pages/Homepage.js
+++ Front-end-React-Bootstrap-JS-CSS-HTML/Ristorante/src/pages/Homepage.js
@@ -4,38 +4,50 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "../components/css/style.css";
 import { MdAccountCircle } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Sidebar } from "../components/Sidebar";
 import { useEffect, useState } from "react";
 import "../components/css/footerStyle.css";
 import { Col, Row } from "react-bootstrap";
-import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineInstagram, AiFillLinkedin } from "react-icons/ai";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { GiSmartphone } from "react-icons/gi";
 import { BiCopyright } from "react-icons/bi";
+import welcome from "../components/img/welcome.jpg";
+
+import Image from "react-bootstrap/Image";
 export const Homepage = () => {
+  const navigate = useNavigate();
   return (
-    <>
+    <Container>
       <Row>
-        <Sidebar />
-      </Row>
-      <Col
-        className="footer mt-5 bg-black text-white text-center fixed-bottom"
-        id="navbar"
-      >
-        <Row className=" d-flex justify-content-between">
-          <Col>
-            <Col className=" text-opacity-100">Contatti:</Col>
-            <BsFillTelephoneFill />
-            08018160035 <GiSmartphone />: 3896492143
-            <Col />
-            <AiOutlineInstagram size={"30px"} />
-            <Col>
-              <BiCopyright /> by Erdison Dosti
+        <Col>
+          <Sidebar />
+        </Col>
+        <Col>
+          
+        </Col>
+        <Col className="footer mt-5 bg-black text-white text-center fixed-bottom bg-opacity-50">
+          <Row className=" d-flex justify-content-between">
+            <Col id="text" className=" me-5">
+              <Col id="text">Contatti:</Col>
+              <BsFillTelephoneFill style={{ color: "white" }} />: 08018160035
+              <GiSmartphone style={{ color: "white" }} />: 3896492143
+              <Col />
+              <AiOutlineInstagram size={"30px"} className=" text-info" />
+              <a
+                href="https://www.linkedin.com/in/erdison-dosti-6943b7180/"
+                target="_blank"
+              >
+                <AiFillLinkedin size={"30px"} className=" text-info" />
+              </a>
+              <Col>
+                <BiCopyright className=" text-info" /> by Erdison Dosti
+              </Col>
             </Col>
-          </Col>
-        </Row>
-      </Col>
-    </>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
